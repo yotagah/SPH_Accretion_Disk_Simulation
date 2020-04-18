df5d196d0af8f1794c7f86cc7e444dc21eb69337
yotagah <jhon.hnng@gmail.com>
2020-04-18 20:27:21 -0300

    English adequation

--- integration.c
+++ integration.c
@@ -21,7 +21,7 @@ void singleStep(particles_t *particles, int_pairs_t *int_pairs, stars_t *stars) 
     pressureAndSoundSpeed(particles, 1.01); // gamma
 
     // Forces
-    zerateForces(particles);
+    resetForces(particles);
     artificialViscosity(particles, int_pairs);
     //artificialHeat(particles, int_pairs);
     internalForce(particles, int_pairs);
@@ -40,7 +40,7 @@ void directFind(particles_t *particles, int_pairs_t *int_pairs) {
     
     scale_k = 2; // Scale factor for: cubic spline kernel by W4 - Spline (Monaghan 1985)
 
-    int_pairs->quant = 0; // Zerate number of interaction pairs
+    int_pairs->quant = 0; // Reset number of interaction pairs
 
     for(i = 0; i < particles->quant; i++) { // All particles
         if(particles->particle[i].active) { // Active particles only
